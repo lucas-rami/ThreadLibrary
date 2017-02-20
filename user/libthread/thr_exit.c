aaeaf9a19202d58c82f6e41a323c9d17fe6e42de
Lucas Ramirez <lramire1@andrew.cmu.edu>
2017-02-20 01:04:59 -0500

    Implemented thr_exit and thr_join

--- user/libthread/thr_exit.c
+++ user/libthread/thr_exit.c
@@ -3,6 +3,9 @@
  *  @author akanjani, lramire1
  */
 
+#include <stdlib.h>
+#include <global_state.h>
+#include <thr_internals.h>
 #include <syscall.h>
 
 /** @brief Exit the thread with an exit status
@@ -13,5 +16,30 @@
  */
 
 void thr_exit( void *status ) {
+
+  // Get the thread's TCB
+  tcb_t* tcb = get_tcb();
+
+  // Should not happen
+  if (tcb == NULL) {
+    //panic("thr_exit(): Thread's TCB does not exist !\n");
+  }
+
+  // Set return status
+  tcb->return_status = status;
+
+  mutex_lock(&tcb->mutex_state);
+
+  if (tcb->thread_state == WAITING_ON) {
+    // Some other thread has called thr_join() on this thread
+      cond_signal(&tcb->cond_var_state);
+  } else {
+    // Set the thread's state to EXITED
+    tcb->thread_state = EXITED;
+  }
+
+  mutex_unlock(&tcb->mutex_state);
+
+  // Vanish the current thread
   vanish();
 }
---
+++ user/libthread/thr_join.c
@@ -0,0 +1,85 @@
+/** @file thr_join.c
+ *  @brief This file contains the definition for the thr_join() function
+ *  @author akanjani, lramire1
+ */
+
+#include <global_state.h>
+#include <hash_table.h>
+#include <stdlib.h>
+#include <syscall.h>
+#include <thr_internals.h>
+
+/** @brief Cleans up after a thread, optionnaly returning the status information
+ *  provided by the thread at the time of exit
+ *
+ *  The target thread tid may or may not have exited before thr join() is
+ *  called; if it has not, the calling thread will be suspended until the target
+ *  thread does exit.
+ *
+ *  If statusp is not NULL, the value passed to thr exit() by the joined thread
+ *  will be placed in the location referenced by statusp.
+ *
+ *  @param tid      Target thread tid
+ *  @param statusp  Pointer to the thread's exit status (void*)
+ *
+ *  @return 0 on success, a negative number on error
+ */
+int thr_join(int tid, void **statusp) {
+
+  // Get the TCB of the thread we want to join on
+  tcb_t *tcb = hash_table_get_element(&task.tcbs, &tid);
+
+  // The thread already exited and was joined on or the tid is invalid
+  if (tcb == NULL) {
+    return -1;
+  }
+
+  mutex_lock(&tcb->mutex_state);
+
+  // If another thread is already waiting, return immediatly
+  if (tcb->thread_state == WAITING_ON) {
+    mutex_unlock(&tcb->mutex_state);
+    return -1;
+  }
+
+  // The thread is still running
+  if (tcb->thread_state == RUNNING) {
+    tcb->thread_state = WAITING_ON;
+    cond_wait(&tcb->cond_var_state, &tcb->mutex_state);
+  }
+
+  // When we get here the thread has exited and we can clean things up
+
+  // We don't need the mutex anymore
+  mutex_unlock(&tcb->mutex_state);
+
+  // Take care of returning the status of the exited thread
+  if (statusp != NULL) {
+    *statusp = tcb->return_status;
+  }
+
+  // Remove TCB from hash table
+  mutex_lock(&task.tcbs_mutex);
+  hash_table_remove_element(&task.tcbs, &tid);
+  mutex_unlock(&task.tcbs_mutex);
+
+  // Deallocate stack pages
+  if (remove_pages(tcb->stack_low) < 0) {
+    // panic("thr_join(): Could not deallocate stack pages !\n");
+  }
+
+  // Mark the deallocated pages as free to use for other threads if this isn't
+  // the root thread
+  if ((unsigned int)tcb->stack_low !=
+      (unsigned int)task.stack_highest_childs + PAGE_SIZE) {
+
+    mutex_lock(&task.queue_mutex);
+    queue_insert_node(&task.stack_queue, tcb->stack_high);
+    mutex_unlock(&task.queue_mutex);
+  }
+
+  // Free the TCB data structure
+  free(tcb);
+
+  return 0;
+}
